b9d0448611fa730afb3a43201c8fdb0490e570a7
Danny Spencer <dan.spencer.np@gmail.com>
2023-06-04 13:10:43 -0700

    Add smb2j ram table

--- ghidra/ghidra-scripts/smb2j_datatypes.py
+++ ghidra/ghidra-scripts/smb2j_datatypes.py
@@ -11,47 +11,45 @@ from ghidra.program.model.data import ByteDataType, PointerDataType, VoidDataTyp
 ByteType = ByteDataType()
 BytePointerType = PointerDataType(ByteType)
 
-def getaddr(mmap, addr_str):
+def getaddr(addr_str):
     # format: 0x1234
     offset = int(addr_str[2:], 16)
     if offset < 0x2000:
         offset += 0x5000
-    
-
 
     return toAddr(offset)
 
-# def create_byte_array(addr, count):
-#     cmd = CreateArrayCmd(addr, count, ByteType, 1)
-#     cmd.applyTo(currentProgram)
+def create_byte_array(addr, count):
+    cmd = CreateArrayCmd(addr, count, ByteType, 1)
+    cmd.applyTo(currentProgram)
 
-# def create_byte_pointer(addr):
-#     createData(addr, BytePointerType)
+def create_byte_pointer(addr):
+    createData(addr, BytePointerType)
 
-# with open(data_filename('smb_ram.csv'), 'r') as f:
-#     contents = f.read().strip()
+with open(data_filename('smb2j_ram.csv'), 'r') as f:
+    contents = f.read().strip()
 
-# items = [item.split(',') for item in contents.split()]
-# # Skip items we don't want the decompiler to use (start with ---)
-# items = [x for x in items if '---' not in x[0]]
-# items.append(('dummy',0x0800,'n/a'))
+items = [item.split(',') for item in contents.split()]
+# Skip items we don't want the decompiler to use (start with ---)
+items = [x for x in items if '---' not in x[0]]
+items.append(('dummy',0x0800,'n/a'))
 
-# for (name,addr,typ),(nextname,nextaddr,nexttyp) in zip(items,items[1:]):
-#     addr = getaddr(addr)
-#     if typ == 'byte':
-#         createLabel(addr, name, True)
-#         createByte(addr)
-#     elif typ == 'pointer':
-#         createLabel(addr, name, True)
-#         create_byte_pointer(addr)
-#         pass
-#     elif typ == 'array':
-#         nextaddr = getaddr(nextaddr)
-#         count = int(nextaddr.offset - addr.offset)
-#         createLabel(addr, name, True)
-#         create_byte_array(addr, count)
-#     else:
-#         pass
+for (name,addr,typ),(nextname,nextaddr,nexttyp) in zip(items,items[1:]):
+    addr = getaddr(addr)
+    if typ == 'byte':
+        createLabel(addr, name, True)
+        createByte(addr)
+    elif typ == 'pointer':
+        createLabel(addr, name, True)
+        create_byte_pointer(addr)
+        pass
+    elif typ == 'array':
+        nextaddr = getaddr(nextaddr)
+        count = int(nextaddr.offset - addr.offset)
+        createLabel(addr, name, True)
+        create_byte_array(addr, count)
+    else:
+        pass
 
 # with open(data_filename('smb_romarrays.csv'), 'r') as f:
 #     contents = f.read().strip()
